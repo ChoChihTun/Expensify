5117831474cd21ea7daf4b96cb14e8e791119dcb
ChoChihTun <33831669+ChoChihTun@users.noreply.github.com>
2018-06-04 17:44:18 +0800

    Set up eslint

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'normalize.css/normalize.css'
+import { BrowserRouter, Route } from 'react-router-dom';
+import 'normalize.css/normalize.css';
+
+
 import './styles/styles.scss';
 
 ReactDOM.render(<p>BoilerPlate</p>, document.getElementById('app'));
