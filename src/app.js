64bcdd9352e913d7771e52c97ba16276b4984dce
ChoChihTun <33831669+ChoChihTun@users.noreply.github.com>
2018-06-04 20:56:37 +0800

    Remove eslint to fix the error

--- src/app.js
+++ src/app.js
@@ -1,18 +1,58 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
 import 'normalize.css/normalize.css';
-<<<<<<< HEAD
-=======
-
-
->>>>>>> origin/master
 import './styles/styles.scss';
+
+const ExpenseDashboardPage = () => (
+  <div>
+    This is from my dashboard component
+  </div>
+);
+
+const AddExpensePage = () => (
+  <div>
+    This is from my add expense component
+  </div>
+);
+
+const EditExpensePage = () => (
+  <div>
+    This is from my edit expense component
+  </div>
+);
+
+const HelpPage = () => (
+  <div>
+    This is from my help component
+  </div>
+);
+
+const NotFoundPage = () => (
+  <div>
+    404! - <Link to='/'>Go Home</Link>
+  </div>
+);
+
+const Header = () => (
+  <header>
+    <h1>Expensify</h1>
+  </header>
+)
 
 const routes = (
   <BrowserRouter>
-
+    <div>
+      <Header />
+      <Switch>
+        <Route path="/" component={ExpenseDashboardPage} exact={true} />
+        <Route path="/create" component={AddExpensePage} />
+        <Route path="/edit" component={EditExpensePage} />
+        <Route path="/help" component={HelpPage} />
+        <Route component={NotFoundPage} />
+      </Switch>
+    </div>
   </BrowserRouter>
-)
+);
 
 ReactDOM.render(routes, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,36 @@
-  const path = require('path');
+const path = require('path');
 
-  module.exports = {
-    entry: {
-      app: './src/app.js',
-    },
-    devtool: 'cheap-module-eval-source-map',
-    devServer: {
-        contentBase: path.resolve(__dirname, 'public')
-    },
-    output: {
-      filename: 'bundle.js',
-      path: path.resolve(__dirname, 'public')
-    },
-    mode: 'development',
-    module: {
-      rules: [{
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader?cacheDirectory=true',
-        }
-      }, 
-      // Any file that ends with .scss
-      {
-        test: /\.s?css$/,
-        use: [
-          "style-loader", // Takes the css in js file and add it to the DOM so user can see
-          "css-loader", // Allow us to use css interpretation in js file such as import and url()
-          "sass-loader" // Allow us to import sass files
-          // MUST install node-sass package too --> helps us converts scss files to regular css file
-        ]
-      }]
-    }
+module.exports = {
+  entry: {
+    app: './src/app.js',
+  },
+  devtool: 'cheap-module-eval-source-map',
+  devServer: {
+    contentBase: path.resolve(__dirname, 'public'),
+    historyApiFallback: true, // Need this for client side routing
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public')
+  },
+  mode: 'development',
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader?cacheDirectory=true',
+      }
+    }, 
+    // Any file that ends with .scss
+    {
+      test: /\.s?css$/,
+      use: [
+        "style-loader", // Takes the css in js file and add it to the DOM so user can see
+        "css-loader", // Allow us to use css interpretation in js file such as import and url()
+        "sass-loader", // Allow us to import sass files
+        // MUST install node-sass package too --> helps us converts scss files to regular css file
+      ]
+    }]
   }
+}
