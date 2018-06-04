5debc6167acb00010e8ad1f6b18b815b1e74cc98
ChoChihTun <33831669+ChoChihTun@users.noreply.github.com>
2018-06-04 21:01:33 +0800

    Add links to header

--- src/app.js
+++ src/app.js
@@ -37,6 +37,10 @@ const NotFoundPage = () => (
 const Header = () => (
   <header>
     <h1>Expensify</h1>
+    <Link to='/'>Dashboard</Link>
+    <Link to='/create'>Create Expense</Link>
+    <Link to='/edit'>Edit Expense</Link>
+    <Link to='/help'>Help</Link>
   </header>
 )
 
