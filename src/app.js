19b8f333969b910396b310799c7f2201e8fb1a9b
ChoChihTun <33831669+ChoChihTun@users.noreply.github.com>
2018-06-04 18:04:36 +0800

    routes

--- src/app.js
+++ src/app.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'normalize.css/normalize.css'
+import { BrowserRouter, Route } from 'react-router-dom';
+import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-ReactDOM.render(<p>BoilerPlate</p>, document.getElementById('app'));
+const routes = (
+  <BrowserRouter>
+
+  </BrowserRouter>
+)
+
+ReactDOM.render(routes, document.getElementById('app'));
