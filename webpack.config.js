93bd59aea29f372847c656be17e034bf2051ccbf
ChoChihTun <33831669+ChoChihTun@users.noreply.github.com>
2018-06-06 10:48:11 +0800

    Add HOC practice

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   entry: {
-    app: './src/app.js',
+    app: './src/Practice/hoc.js',
   },
   devtool: 'cheap-module-eval-source-map',
   devServer: {
