48d7176d7c90a6e12376a189432cbe9b0c66d662
ChoChihTun <33831669+ChoChihTun@users.noreply.github.com>
2018-06-09 15:54:44 +0800

    Add edit page and form

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,12 +1,32 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import ExpenseForm from './ExpenseForm';
+import { removeExpense, editExpense } from '../actions/expenses';
 
-const EditExpensePage = (props) => {
-  console.log(props);
-  return (
+const EditExpensePage = (props) => (
   <div>
-    Editing the expense with id of {props.match.params.id}
+    <ExpenseForm
+      expense={props.expense}
+      onSubmit={(expense) => {
+        props.dispatch(editExpense(props.expense.id, expense));
+        props.history.push('/');
+      }}
+    />
+    <button onClick={() => {
+      props.dispatch(removeExpense({ id: props.expense.id }));
+      props.history.push('/');
+    }}>Remove
+    </button>
   </div>
-  );
-};
+);
 
-export default EditExpensePage;
+// props in the args is the info that is passed from the ExpenseListItem page
+const mapStateToProps = (state, props) => {
+  return {
+    expense: state.expenses.find((expense) => (expense.id === props.match.params.id))
+  }
+}
+
+// Need to connect to Redux store to find the specific expense
+// Need to retrieve data so we need to map state to props
+export default connect(mapStateToProps)(EditExpensePage);
