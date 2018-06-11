da6914efb9144c3d9a186f6f93301f9e1662c013
ChoChihTun <33831669+ChoChihTun@users.noreply.github.com>
2018-06-11 14:35:32 +0800

    Setup production webpack build and extract-text-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,18 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = (env) => {
   // Must run -p --env production in package.json
   const isProduction = env === 'production';
+  // For extracting all css files into a common file
+  const CSSExtract = new ExtractTextPlugin('styles.css');
 
   return {
     entry: {
       app: './src/app.js',
     },
     // Different source map for dev & prod
-    devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
+    devtool: isProduction ? 'source-map' : 'inline-source-map',
     devServer: {
       contentBase: path.resolve(__dirname, 'public'),
       historyApiFallback: true, // Need this for client side routing
@@ -30,13 +33,30 @@ module.exports = (env) => {
       // Any file that ends with .scss
       {
         test: /\.s?css$/,
-        use: [
-          "style-loader", // Takes the css in js file and add it to the DOM so user can see
-          "css-loader", // Allow us to use css interpretation in js file such as import and url()
-          "sass-loader", // Allow us to import sass files
-          // MUST install node-sass package too --> helps us converts scss files to regular css file
-        ]
+        use: CSSExtract.extract({
+          use: [
+            // Put loader in object so that we can use source map to check the original code location and not the styles.css in Browser's Inpect Element 
+            {
+              // Allow us to use css interpretation in js file such as import and url()
+              loader: 'css-loader',
+              options: {
+                sourceMap: true
+              }
+            },
+            {
+              // Allow us to import sass files
+              // MUST install node-sass package too --> helps us converts scss files to regular css file
+              loader: 'sass-loader',
+              options: {
+                sourceMap: true
+              }
+            }
+          ]
+        })
       }]
-    }
+    },
+    plugins: [
+      CSSExtract
+    ]
   }
 }
