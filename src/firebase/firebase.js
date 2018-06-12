59d85109ad2269e1584663b3b66ad8351b574705
ChoChihTun <33831669+ChoChihTun@users.noreply.github.com>
2018-06-12 23:26:24 +0800

    Separate database for development and for testing

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,12 +3,12 @@ import * as firebase from 'firebase';
 
 // Initialize Firebase
 var config = {
-  apiKey: "AIzaSyDNiySl5cG1SYE4i2P2itJW1lh9H3VbS-4",
-  authDomain: "expensify-1bf6d.firebaseapp.com",
-  databaseURL: "https://expensify-1bf6d.firebaseio.com",
-  projectId: "expensify-1bf6d",
-  storageBucket: "expensify-1bf6d.appspot.com",
-  messagingSenderId: "529777249150"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 };
 
 // Initialise firebase to work with
